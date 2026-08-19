2520d4be9ee0b1584fc6097310cf311a3eee0cbd
bhaskarBabu123 <142469090+bhaskarBabu123@users.noreply.github.com>
2026-08-19 17:43:58 +0530

    Change logo image width from 80 to 60 pixels

--- src/components/public/UnifiedHeader.jsx
+++ src/components/public/UnifiedHeader.jsx
@@ -42,7 +42,7 @@ const UnifiedHeader = () => {
           {/* LOGO */}
           <div className="flex items-center gap-10">
             <Link to="/" className="flex items-center gap-2 group">
-              <img src={logo} alt="" width="80" />
+              <img src={logo} alt="" width="60" />
             </Link>
 
             {/* DESKTOP NAV: Combines Public + User Links */}
